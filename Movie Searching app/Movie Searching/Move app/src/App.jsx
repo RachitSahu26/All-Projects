74d94c87a021b660706ff1e953cccc82068c09e9
rachit <curiousrachit.26@gmail.com>
2024-01-08 13:54:23 +0530

    wrapped whole code inside myState

--- Movie Searching app/Movie Searching/Move app/src/App.jsx
+++ Movie Searching app/Movie Searching/Move app/src/App.jsx
@@ -2,21 +2,26 @@ import React from 'react'
 import NavBar from './Components/NavBar/NavBar'
 import SearchBar from './Components/SearchBar/SearchBar'
 import CardArea from './Components/CartArea.jsx/CardArea'
+import MyState from './Context/Data/MyState'
 // import MoveCard from './Components/Movie Card/MoveCard'
-
 function App() {
   return (
-    <div>
+    <MyState>
 
-      <NavBar />
 
-      <div className="bg">
-        <SearchBar />
-        <CardArea />
+      <div>
+
+        <NavBar />
+
+        <div className="bg">
+          <SearchBar />
+          <CardArea />
+
+        </div>
 
       </div>
 
-    </div>
+    </MyState>
   )
 }
 
