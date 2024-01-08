ac19c25142a9d5937ac7a29848f532cd95a87839
rachit <curiousrachit.26@gmail.com>
2024-01-08 13:53:14 +0530

    Created Search Functionality

--- Movie Searching app/Movie Searching/Move app/src/Components/SearchBar/SearchBar.jsx
+++ Movie Searching app/Movie Searching/Move app/src/Components/SearchBar/SearchBar.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import mycontext from '../../Context/Data/MyContext';
 
-function SearchBar({ placeholder, onChange }) {
+function SearchBar({ placeholder}) {
+
+ const contexdata= useContext(mycontext);
+const {movieSearch, setMovieSearch,fetchData }=contexdata
   return (
     <div className="md:flex items-center  mt-3 max-w-md mx-auto p-2 bg-yellow-400 rounded-full shadow-md">
     <input
       type="text"
       placeholder={placeholder || 'Search...'}
+      value={movieSearch}
       className="flex-1 py-2 px-4 rounded-full focus:outline-none mb-2 md:mb-0 md:mr-2"
-      onChange={onChange}
+      onChange={(e)=>setMovieSearch(e.target.value)}
     />
-    <button className="p-2 bg-red-500 text-white rounded-full">
+    <button className="p-2 bg-red-500 text-white rounded-full"
+    onClick={fetchData}
+    >
       Search
     </button>
   </div>
