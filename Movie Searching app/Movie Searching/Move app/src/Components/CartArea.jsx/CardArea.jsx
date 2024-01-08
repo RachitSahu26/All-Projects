661bc22f496f4de6f9ff3020c571e06ca2efb633
rachit <curiousrachit.26@gmail.com>
2024-01-08 13:53:59 +0530

    data maped

--- Movie Searching app/Movie Searching/Move app/src/Components/CartArea.jsx/CardArea.jsx
+++ Movie Searching app/Movie Searching/Move app/src/Components/CartArea.jsx/CardArea.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react';
 import MoveCard from '../Movie Card/MoveCard';
+import mycontext from '../../Context/Data/MyContext';
 
 function CardArea() {
+    const contextData = useContext(mycontext);
+    const { allMovieData } = contextData;
+
     return (
-        <div className='  mt-2 flex  flex-wrap  '>
-            <MoveCard />
-            <MoveCard />
-            <MoveCard />
-            <MoveCard />
-            <MoveCard />
-            <MoveCard />
-            <MoveCard />
-            <MoveCard />
-            <MoveCard />
+        <div className='mt-2 flex flex-wrap'>
+            {allMovieData.map((item, index) => (
+                // Use parentheses here instead of curly braces
+                <MoveCard
+                    key={index}  // Add a unique key for each item in the map
+                    title={item.Title}
+                    poster={item.Poster}
+                    year={item.Year}
+                />
+            ))}
         </div>
     );
 }
 
-export default CardArea
+export default CardArea;
