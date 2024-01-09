fe00b56739b0e577d3c78bda59a82a9342a12190
rachit <curiousrachit.26@gmail.com>
2024-01-09 19:31:36 +0530

    created Loading state

--- Movie Searching app/Movie Searching/Move app/src/Context/Data/MyState.jsx
+++ Movie Searching app/Movie Searching/Move app/src/Context/Data/MyState.jsx
@@ -5,28 +5,38 @@ function MyState(props) {
 
     const [movieSearch, setMovieSearch] = useState("");
     // const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
     const [allMovieData, setAllMovieData] = useState([]);
+
+// .....................specific selected movie data state .........
+const [selectedMovieData,setSelectedMovieData]=useState([]);
+
 
     const fetchData = async () => {
         try {
-            // setLoading(true);
+            setLoading(true);
             const res = await fetch(`http://www.omdbapi.com/?s=${movieSearch}&apikey=cae6e323`)
             const data = await res.json();
             setAllMovieData(data.Search);
             console.log(data.Search)
-
-            // setLoading(false);
+            setMovieSearch("");
+            setLoading(false);
 
 
         } catch (error) {
             console.log(error)
-            // setLoading(false)
+            setLoading(false)
         }
 
     }
 
     return (
-        <MyContext.Provider value={{ fetchData, movieSearch, setMovieSearch, allMovieData, setAllMovieData }}>
+        <MyContext.Provider value={{ fetchData,
+         movieSearch, setMovieSearch,
+          allMovieData, setAllMovieData, 
+          selectedMovieData,setSelectedMovieData,
+          loading, setLoading
+           }}>
 
             {props.children}
 
