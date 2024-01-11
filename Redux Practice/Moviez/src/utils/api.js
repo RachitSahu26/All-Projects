b5ca6676db2381e5512744b34d6b4465b8d6f1a4
rachit <curiousrachit.26@gmail.com>
2024-01-11 22:10:47 +0530

    created api calling function

---
+++ Redux Practice/Moviez/src/utils/api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+const TMDB_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZDdkODE2MmYwYTE2ZmFlMDI5MzkwMmJmMzJiNjU2MSIsInN1YiI6IjY0YmViOTFjYjg2NWViMDExY2EwZDYxNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4-by30aCD9EUe2jjUAgYik-X3Osqir9Atg8VOSrNtdU"
+const headers = {
+    Authorization: "bearer " + TMDB_TOKEN,
+
+}
+export const fetchDataFromApi = async (url, params) => {
+    try {
+        const { data } = await axios.get(BASE_URL + url, {
+            headers,
+            params,
+        });
+        return data;
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+};
