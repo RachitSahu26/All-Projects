64d2b4d0c5678debed32819a89fa1108a2b5e2b1
rachit <curiousrachit.26@gmail.com>
2024-01-18 22:16:46 +0530

    changed navbar

--- Movie Searching app/Movie Searching/Move app/src/Components/NavBar/NavBar.jsx
+++ Movie Searching app/Movie Searching/Move app/src/Components/NavBar/NavBar.jsx
@@ -1,69 +1,72 @@
+// Navbar.jsx
+
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
-const NavBar = () => {
-    const [isOpen, setIsOpen] = useState(false);
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleNavbar = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <nav className="bg-black static top-0  p-4">
-            <div className="container mx-auto flex justify-between items-center">
-                <div className="text-white font-bold  text-xl">Movie Search</div>
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white font-bold text-xl"> <a href='/'>Movie Search</a></div>
 
-                <button
-                    className="lg:hidden text-white focus:outline-none"
-                    onClick={toggleNavbar}
-                >
-                    <svg
-                        className="h-6 w-6"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M4 6h16M4 12h16m-7 6h7"
-                        ></path>
-                    </svg>
-                </button>
+        <button
+          className="lg:hidden text-white focus:outline-none"
+          onClick={toggleNavbar}
+        >
+          <svg
+            className="h-6 w-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            ></path>
+          </svg>
+        </button>
 
-                <div
-                    className={`lg:flex flex-grow items-center ${isOpen ? 'block' : 'hidden'
-                        }`}
-                >
-                    <div className="lg:flex lg:mr-4   ">
-                        <a
-                            href="#"
-                            className="block lg:inline-block mt-4 lg:mt-0 text-white hover:text-gray-300 mr-4"
-                        >
-                            Home
-                        </a>
-                        <a
-                            href="#"
-                            className="block lg:inline-block mt-4 lg:mt-0 text-white hover:text-gray-300 mr-4"
-                        >
-                            Movies
-                        </a>
-
-                        <a
-                           className="block lg:inline-block mt-4 lg:mt-0 text-white hover:text-gray-300"
-                        >
-                            about
-                        </a>
-                       
-
-                    </div>
-
-                </div>
-            </div>
-        </nav>
-    );
+        <div
+          className={`lg:flex lg:items-center ${
+            isOpen ? 'block' : 'hidden'
+          }`}
+        >
+          <div className="lg:flex lg:mr-4">
+            <a
+              href="/trending"
+              className="block lg:inline-block mt-4 lg:mt-0 text-white hover:text-gray-300 mr-4"
+            >
+              Trending
+            </a>
+            <a
+              href="/movies"
+              className="block lg:inline-block mt-4 lg:mt-0 text-white hover:text-gray-300 mr-4"
+            >
+              Movies
+            </a>
+            <a
+              href="/tv_show"
+              className="block lg:inline-block mt-4 lg:mt-0 text-white hover:text-gray-300"
+            >
+              TV Show
+            </a>
+          </div>
+          <div>
+            {/* Add additional links or components here */}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
-export default NavBar;
+export default Navbar;
