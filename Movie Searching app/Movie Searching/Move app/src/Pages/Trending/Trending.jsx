8a0d7c971dfcdb0c6d8d7aeafa81683c37e31ee9
rachit <curiousrachit.26@gmail.com>
2024-01-20 22:27:21 +0530

    changed

--- Movie Searching app/Movie Searching/Move app/src/Pages/Trending/Trending.jsx
+++ Movie Searching app/Movie Searching/Move app/src/Pages/Trending/Trending.jsx
@@ -3,26 +3,42 @@ import React, { useEffect, useState } from 'react'
 import MoveCard from '../../Components/Movie Card/MoveCard'
 import Navbar from '../../Components/NavBar/NavBar'
 import Pages from '../../Components/Pagination/Pages'
+// import Pages from '../../Components/Pagination/Pages'
 
 function Trending() {
 
   const [trendContent, setTrendContent] = useState([])
 
-
+  const [page, setPage] = useState(() => {
+    // Get the current page from localStorage or default to 1
+    const storedPage = localStorage.getItem('currentPages');
+    return storedPage ? parseInt(storedPage, 10) : 1;
+  })
   const fetchTrending = async () => {
 
+    try {
 
-    const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=4e44d9029b1270a757cddc766a1bcb63`
-    )
+      const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=4e44d9029b1270a757cddc766a1bcb63&page=${page}`
+      )
 
-    setTrendContent(data.results);
-    console.log(data)
+      setTrendContent(data.results);
+      console.log(data)
+      // Save the current page to localStorage
+      localStorage.setItem('currentPages', page.toString());
+    } catch (error) {
+      console.error('Error fetching movie data:', error);
 
+    }
   }
   useEffect(() => {
     fetchTrending();
-  }, [])
+  }, [page])
 
+
+
+  const SelectedPageHandler = (SelectedPage) => {
+    setPage(SelectedPage)
+  }
   return (
     <>
       <div className='flex flex-wrap'>
@@ -45,10 +61,10 @@ function Trending() {
 
 
         }
-    <div className='flex justify-center items-center ' >
+        <div className='flex bg-black p-5 w-full justify-center items-center' >
 
-<Pages/>
-</div>
+          <Pages movie_content={trendContent} NumberofPage={page} Selected_Page_Handler={SelectedPageHandler} />
+        </div>
       </div>
     </>
 
