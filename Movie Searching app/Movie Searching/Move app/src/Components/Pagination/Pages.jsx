8f5141e3eebaf6e50940e7ec255754561971f142
rachit <curiousrachit.26@gmail.com>
2024-01-20 22:24:45 +0530

    completed pagination functionality

--- Movie Searching app/Movie Searching/Move app/src/Components/Pagination/Pages.jsx
+++ Movie Searching app/Movie Searching/Move app/src/Components/Pagination/Pages.jsx
@@ -1,64 +1,25 @@
-// import React from 'react';
+import React from 'react';
 
-// function Pages({ currentPage, totalPage, onPageChange }) {
-//   const pageNumbers = Array.from({ length: totalPage }, (_, index) => index + 1);
+function Pages({ movie_content, NumberofPage, Selected_Page_Handler }) {
+    return (
+        <>
+            <div className='text-2xl bg-red-100 m-5 px-3 py-1 rounded w-30 h-10'>
+                <span className='cursor-pointer' onClick={() => Selected_Page_Handler(NumberofPage + 1)}>⬅️</span>
+           
+                {Array.from({ length: Math.ceil(movie_content.length / 2) }, (_, i) => (
+                    <span
+                        className={`bg-black-500 p-1 cursor-pointer hover:bg-yellow-500 ${NumberofPage === i + 1 ? 'bg-yellow-500' : 'bg-white'
+                            }`}
+                        key={i}
+                        onClick={() => Selected_Page_Handler(i + 1)}
+                    >
+                        {i + 1}
+                    </span>
+                ))}
+                <span className='bg-white cursor-pointer' onClick={() => Selected_Page_Handler(NumberofPage + 1)}>➡️</span>
+            </div>
+        </>
+    );
+}
 
-//   return (
-//     <>
-//       <nav aria-label="Page navigation example">
-
-
-//         <ul className="inline-flex -space-x-px text-md">
-
-//           {/* ...............1................ */}
-//           <li>
-
-//             <a
-//               href="#"
-//               className={`flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${currentPage === 1 ? 'pointer-events-none' : ''}`}
-//               onClick={() => onPageChange(currentPage - 1)}
-//             >
-//               Previous
-//             </a>
-
-//           </li>
-
-//           {/* ...............2................ */}
-
-//           {pageNumbers.slice(0, 10).map((pageNumber) => (
-//             <li key={pageNumber}>
-//               <a
-//                 href="#"
-//                 className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${currentPage === pageNumber ? 'text-blue-600 border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white' : ''}`}
-//                 onClick={() => onPageChange(pageNumber)}
-//               >
-//                 {pageNumber}
-//               </a>
-//             </li>
-//           ))}
-
-
-
-
-//           {/* ...............3............... */}
-//           <li>
-//             <a
-//               href="#"
-//               className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${currentPage === totalPage ? 'pointer-events-none' : ''}`}
-//               onClick={() => onPageChange(currentPage + 1)}
-//             >
-//               Next
-//             </a>
-//           </li>
-
-
-
-
-
-//         </ul>
-//       </nav>
-//     </>
-//   );
-// }
-
-// export default Pages;
+export default Pages;
