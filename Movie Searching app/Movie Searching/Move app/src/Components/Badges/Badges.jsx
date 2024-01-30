6f2cb560792f01e51b69efee8999b4d75f929724
rachit <curiousrachit.26@gmail.com>
2024-01-30 23:31:39 +0530

    badges has been completed

--- Movie Searching app/Movie Searching/Move app/src/Components/Badges/Badges.jsx
+++ Movie Searching app/Movie Searching/Move app/src/Components/Badges/Badges.jsx
@@ -1,19 +1,17 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-function Badges({ type }) {
-  
+function Badges({ type, onGenreClick }) {
   const [genres, setGenres] = useState([]);
-  const [selectedGenres, setSelectedGenres] = useState([]);
-
+  const [selectedGenre, setSelectedGenre] = useState(null);
 
   const fetchGenres = async () => {
     try {
       const { data } = await axios.get(
         `https://api.themoviedb.org/3/genre/${type}/list?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`
       );
-      console.log(data.genres); // Check if genres are nested under the 'genres' property
-      setGenres(data.genres); // Set genres to the correct property
+      console.log(data.genres);
+      setGenres(data.genres);
     } catch (error) {
       console.error('Error fetching genres:', error);
     }
@@ -23,12 +21,21 @@ function Badges({ type }) {
     fetchGenres();
   }, []);
 
+  const handleGenreClick = (genre) => {
+    // Toggle selection on click
+    setSelectedGenre((prevGenre) => (prevGenre === genre.id ? null : genre.id));
+    onGenreClick(genre.id);
+  };
+
   return (
     <>
       {genres.map((genre) => (
         <span
           key={genre.id}
-          className={`bg-white cursor-pointer text-black text-2xl font-medium mr-2 px-2.5  py-0.5 rounded `}
+          className={`cursor-pointer text-black text-2xl font-medium mr-2 px-2.5 py-0.5 rounded ${
+            selectedGenre === genre.id ? 'bg-red-500' : 'bg-white'
+          }`}
+          onClick={() => handleGenreClick(genre)}
         >
           {genre.name}
         </span>
