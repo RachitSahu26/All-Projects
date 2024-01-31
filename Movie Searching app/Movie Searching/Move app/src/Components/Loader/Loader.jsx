ac80be39eca89c0e3ec7f30b0ad6c4fab8f5573a
rachit <curiousrachit.26@gmail.com>
2024-02-01 00:13:05 +0530

    created loader compo

--- Movie Searching app/Movie Searching/Move app/src/Components/Loader/Loader.jsx
+++ Movie Searching app/Movie Searching/Move app/src/Components/Loader/Loader.jsx
@@ -1,27 +1,20 @@
-// Loader.js
-import React, { useCallback, useContext, useEffect } from 'react';
-import mycontext from '../../Context/Data/MyContext';
-const Loader = () => {
-    
- const context =useContext(mycontext);
- const {loading, setLoading}=context;
-    useEffect(() => {
-      // Simulate an asynchronous operation
-      const delay = setTimeout(() => {
-        setLoading(false);
-      }, 3000);
-  
-      return () => clearTimeout(delay);
-    }, []);
+import React from 'react';
 
-
-
-
+export default function Loader() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="animate-spin rounded-full border-t-4 border-b-4 border-white h-12 w-12"></div>
+      <div role="status">
+        <svg
+          aria-hidden="true"
+          className="inline w-16 h-16 mr-2 text-gray-200 animate-spin dark:text-gray-600"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="2" />
+        </svg>
+        <span className="sr-only">Loading...</span>
+      </div>
     </div>
   );
-};
-
-export default Loader;
+}
