7f14c8693b632456213059427e19fd6f7b742c6c
rachit <curiousrachit.26@gmail.com>
2024-02-15 23:13:30 +0530

    install toaster

--- Ecommerce/client/src/Components/Layout/LayOut.jsx
+++ Ecommerce/client/src/Components/Layout/LayOut.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
-
+import { ToastContainer } from 'react-toastify'
 const LayOut = (props) => {
   return (
     <div>
       <Header/>
+
       <main>
 {props.children}
+<ToastContainer/>
+
       </main>
    <Footer/>
     </div>
