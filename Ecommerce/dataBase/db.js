dde86732de7d7ba9f3664c2fec5e4765ec03345f
rachit <curiousrachit.26@gmail.com>
2024-02-15 23:15:04 +0530

    increased Database connection

--- Ecommerce/dataBase/db.js
+++ Ecommerce/dataBase/db.js
@@ -1,13 +1,17 @@
-import { connect } from "mongoose";
+import mongoose from "mongoose";
 
 const connectToMongo = async () => {
   try {
-await connect('mongodb+srv://curiousrachit26:curious2005261@cluster0.cmdu8dc.mongodb.net/');
-// mongodb+srv://curiousrachit26:curious2005261@cluster0.cmdu8dc.mongodb.net/notebook
-    console.log("---***Database Connected Successfully***---")
+    await mongoose.connect('mongodb+srv://curiousrachit26:curious2005261@cluster0.cmdu8dc.mongodb.net/', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      connectTimeoutMS: 30000, // Increase the connection timeout
+      socketTimeoutMS: 45000, // Increase the socket timeout
+    });
+    console.log("---***Database Connected Successfully***---");
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 export default connectToMongo;
