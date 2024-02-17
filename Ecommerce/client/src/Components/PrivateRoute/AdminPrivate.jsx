656a33db7b8a90f0fe8a307c547f956dff5c8ea4
rachit <curiousrachit.26@gmail.com>
2024-02-17 22:45:03 +0530

    Admin private

---
+++ Ecommerce/client/src/Components/PrivateRoute/AdminPrivate.jsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import mycontext from "../../Context/myContext";
+import { Outlet } from 'react-router-dom';
+import Spinner from "../Spinner/Spinner";
+
+export const AdminPrivate = () => {
+    const { auth } = useContext(mycontext);
+    const [authorized, setAuthorized] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const authCheck = async () => {
+            try {
+                if (auth?.token) {
+                    const res = await axios.get("http://localhost:3000/api/auth/admin-auth", {
+                        headers: {
+                            "Authorization": auth?.token
+                        }
+                    });
+                    setAuthorized(res.data.ok);
+                } else {
+                    // If no token is available, consider the user unauthorized
+                    setAuthorized(false);
+                }
+            } catch (error) {
+                console.error("Error checking admin authorization:", error);
+                setAuthorized(false);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        authCheck();
+    }, [auth]);
+
+    if (loading) {
+        // Render loading spinner while checking authorization
+        return <Spinner />;
+    }
+
+    // Render outlet if user is authorized, otherwise show unauthorized message
+    return authorized ? <Outlet /> : <div>Unauthorized access</div>;
+};
