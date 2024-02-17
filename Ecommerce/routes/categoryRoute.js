979fa0edc07d5dc593be9ec7b197e1a8293d49e7
rachit <curiousrachit.26@gmail.com>
2024-02-17 22:47:22 +0530

    category route

---
+++ Ecommerce/routes/categoryRoute.js
@@ -0,0 +1,47 @@
+import express from "express";
+import { isAdmin, requireSignIn } from "../middleware/SignInRequrie.js";
+import createCategoryController, { categoryControlller, deleteCategoryController, updateCategoryController } from "../controllers/createCategoryController.js";
+
+
+const router = express.Router()
+
+
+
+
+//routes
+// create category
+router.post(
+    "/create-category",
+    requireSignIn,
+    createCategoryController,
+);
+
+
+
+
+
+
+// update category
+router.put(
+    "/update-category/:id",
+    requireSignIn,
+    isAdmin,
+    updateCategoryController
+);
+
+
+//getALl category
+router.get("/get-category", categoryControlller);
+
+
+
+//delete category
+router.delete("/delete-category/:id",
+    requireSignIn,
+    isAdmin,
+    deleteCategoryController
+)
+
+
+
+export default router;
