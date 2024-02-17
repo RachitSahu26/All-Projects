955d94e300b742a7bf89d7f0e7716dd7deabeb44
rachit <curiousrachit.26@gmail.com>
2024-02-17 22:46:32 +0530

    added category state

--- Ecommerce/client/src/Context/mystate.jsx
+++ Ecommerce/client/src/Context/mystate.jsx
@@ -1,35 +1,98 @@
 import React, { useEffect, useState } from 'react'
 import mycontext from './myContext'
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Mystate = (props) => {
-
-    const [auth, setAuth] = useState({
-        user:null,
-        token: ""
-    });
+  const [name, setName] = useState("");
+  const [categories, setCategories] = useState([])
 
 
-    useEffect(() => {
-        const data = localStorage.getItem("auth",)
-        if (data) {
-            const parseData = JSON.parse(data);
-            setAuth({
-                ...auth,
-                token: parseData.token,
-                user:parseData.user,
-            })
 
 
+  const [auth, setAuth] = useState({
+    user: null,
+    token: ""
+  });
+
+
+  useEffect(() => {
+    const data = localStorage.getItem("auth",)
+    if (data) {
+      const parseData = JSON.parse(data);
+      setAuth({
+        ...auth,
+        token: parseData.token,
+        user: parseData.user,
+      })
+
+
+    }
+  }, []);
+
+  // ...................................get all category data...........
+
+  const getAllCategory = async () => {
+   
+    try {
+      const { data } = await axios.get("http://localhost:3000/api/category/get-category",);
+
+      if (data?.success) {
+        toast.success(`successfully  created`);
+        setCategories(data?.category);
+      } else {
+        toast.error(data.message);
+      }
+
+
+    } catch (error) {
+      console.log(error);
+      toast.error("somthing went wrong in input form");
+    }
+  }
+
+
+  const categoryHandle = async () => {
+    try {
+      const { data } = await axios.post(
+        "http://localhost:3000/api/category/create-category",
+        {
+          name,
+        },
+        {
+          headers: {
+            Authorization: auth?.token,
+          },
         }
-    },[]);
+      );
+      if (data?.success) {
+        toast.success(`${name} is created`);
+        getAllCategory();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong in input form");
+    }
+  };
 
 
 
-    return (
-        <mycontext.Provider value={{ auth, setAuth }}>
-            {props.children}
-        </mycontext.Provider>
-    )
+
+
+
+
+
+  return (
+    <mycontext.Provider value={{
+      auth, setAuth,
+      name, setName, categoryHandle,
+      categories, setCategories,  getAllCategory
+    }}>
+      {props.children}
+    </mycontext.Provider>
+  )
 }
 
 export default Mystate
