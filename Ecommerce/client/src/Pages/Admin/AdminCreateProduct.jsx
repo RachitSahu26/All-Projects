c2bdb8c1ae046cda8c165213950ee41e0a1b971c
rachit <curiousrachit.26@gmail.com>
2024-02-22 22:49:05 +0530

    create product  function  calling here

--- Ecommerce/client/src/Pages/Admin/AdminCreateProduct.jsx
+++ Ecommerce/client/src/Pages/Admin/AdminCreateProduct.jsx
@@ -1,41 +1,27 @@
-import React, { useContext } from 'react'
-import LayOut from '../../Components/Layout/LayOut.jsx'
-import Sidebar from '../../Components/SideBar/AdminSideBar.jsx'
-import ProductForm from '../../Components/Product_Form/ProductForm.jsx'
-import mycontext from '../../Context/myContext.jsx'
-// import Sidebar from '../../Components/SideBar/UserSideBar.jsx'
+import React, { useContext, useEffect, useState } from 'react';
+import LayOut from '../../Components/Layout/LayOut.jsx';
+import Sidebar from '../../Components/SideBar/AdminSideBar.jsx';
+import mycontext from '../../Context/myContext.jsx';
+import ProductForm from '../../Components/Product_Form/ProductForm.jsx';
 
 const AdminCreateProduct = () => {
-
-
-
-  const contextData = useContext(mycontext);
-  const { categories, setCategories, getAllCategory } = contextData
-  useEffect(() => {
-    getAllCategory()
-  }, [])
-
-
-
+  
   return (
     <LayOut>
       <div className='border-2 border-yellow-700 flex flex-row'>
         <div>
-
           <Sidebar />
         </div>
-
-
-
         <div className='bg-red-100 w-[90%] justify-center'>
-          <h1> Create Product </h1>
-          <ProductForm />
+         
+          <div className='bg-yellow-200 p-5'>
+            <h1>Add form</h1>
+            <ProductForm />
+          </div>
         </div>
-
-
       </div>
     </LayOut>
-  )
-}
+  );
+};
 
-export default AdminCreateProduct
+export default AdminCreateProduct;
