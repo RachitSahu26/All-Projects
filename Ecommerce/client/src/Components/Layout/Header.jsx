8099c07ff8026c1b48424a5c001411e3139624e8
rachit <curiousrachit.26@gmail.com>
2024-03-18 22:37:35 +0530

    Responsive Completed

--- Ecommerce/client/src/Components/Layout/Header.jsx
+++ Ecommerce/client/src/Components/Layout/Header.jsx
@@ -1,176 +1,162 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import mycontext from '../../Context/myContext';
 import { toast } from 'react-toastify';
 import { useSelector } from 'react-redux';
-
+import { FaShoppingCart, FaBars, FaTimes, FaList, FaHome, FaUser, FaUserPlus } from 'react-icons/fa';
+// import { FaBars,  } from 'react-icons/fa';
 function Header() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const ContextData = useContext(mycontext);
   const cartItem = useSelector((state) => state.cart);
   const { auth, setAuth, categories, getAllCategory } = ContextData;
-  // const [isOpenNav, setIsOpenNav] = useState(false);
 
-  // const toggleNavbars = () => {
-  //   setIsOpenNav(!isOpenNav);
-  // };
-  const [isOpen, setIsOpen] = useState(false);
-
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
   const [isCategoryMenuOpen, setIsCategoryMenuOpen] = useState(false);
-
-
-  const toggleCategoryMenu = () => {
-    setIsCategoryMenuOpen(!isCategoryMenuOpen);
-  };
 
   const logOutHandle = () => {
     setAuth({
       ...auth,
       user: null,
       token: ""
-    })
+    });
     localStorage.removeItem("auth");
     navigate('/signup');
     toast.success("Logout Successfully");
-  }
-
-
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
   };
 
   useEffect(() => {
-    getAllCategory()
-  }
-    , [])
+    getAllCategory();
+  }, []);
+
+  const toggleMobileNav = () => {
+    setIsMobileNavOpen(!isMobileNavOpen);
+  };
+  const toggleCategoryMenu = () => {
+    setIsCategoryMenuOpen(!isCategoryMenuOpen);
+  };
+
   return (
     <>
-      <nav className="bg-gray-800  ">
-
-
-
-        <div className="max-w-7xl   mx-auto px-4">
-
-
-
-          <div className=" h-16">
-            <div className="flex justify-between">
-              {/* ........................logo................ */}
-              <div className="flex-shrink-0 flex items-center">
-                <span className="text-teal-100 text-xl font-bold">Logo</span>
-              </div>
-
-
-              <div className="hidden md:block">
-                <div className="ml-10 flex items-baseline space-x-4">
-                  <Link to="/" className="text-gray-300 hover:text-white  p-4 rounded-md text-xl font-medium">Home</Link>
-
-
-                  {/* .....................category............. */}
-                  <div className="relative">
-                    <button
-                      onClick={toggleCategoryMenu}
-                      className="text-gray-300 hover:text-white p-4 rounded-md text-xl font-medium focus:outline-none"
-                    >
-                      Category
-                    </button>
-                    {isCategoryMenuOpen && (
-                      <div className="absolute z-10 right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        {/* Render the category names here */}
-                        <ul>
-                          <Link to={`/category`} className="block px-4 py-2 text-gray-700 text-sm hover:bg-gray-100">All Category</Link>
-
-                          {categories.map((category) => (
-                            <li key={category._id}  >
-                              <Link to={`/category/${category.slug}`} className="block px-4 py-2 text-gray-700 text-sm hover:bg-gray-100">{category.name}</Link>
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    )}
-                  </div>
-
-
-
-
-
-
-                  {
-                    !auth.user ? (
-                      <>
-                        <Link to="/signup" className="text-gray-300 hover:text-white p-4 rounded-md text-xl font-medium">SignUp</Link>
-                        <Link to="/signin" className="text-gray-300 hover:text-white p-4 rounded-md text-xl font-medium">SignIn</Link>
-
-                      </>
-                    ) : (
-
-                      <>
-
-
-                        <div className="relative inline-block text-left">
-                          <button
-                            type="button"
-                            className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                            onClick={toggleMenu}
-                          >
-                            Options
-                            <svg
-                              className={`${isOpen ? '-mr-1' : 'mr-1'} h-5 w-5 text-gray-400`}
-                              viewBox="0 0 20 20"
-                              fill="currentColor"
-                              aria-hidden="true"
-                            >
-                              <path
-                                fillRule="evenodd"
-                                d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                                clipRule="evenodd"
-                              />
-                            </svg>
-                          </button>
-
-                          {isOpen && (
-                            <ul className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                              <li>
-                                <Link to={`/dashboard/${auth?.user?.role === 1 ? "admin" : "user"}`} className="text-gray-700 block px-4 py-2 text-sm">
-                                  Dashboard
-                                </Link>
-                              </li>
-                              <li>
-                                <Link onClick={logOutHandle} className="text-gray-700 block px-4 py-2 text-sm">
-                                  Logout
-                                </Link>
-                              </li>
-
-
-                            </ul>
-                          )}
-                        </div>
-
-
-
-
-                      </>
-                    )
-                  }
-
-
-
-                  <Link to={"/cart"} className="text-gray-300 hover:text-white p-4 rounded-md text-xl font-medium" >Cart{cartItem.length}</Link>
-
-                </div>
-              </div>
-
-
+      <nav className="bg-gray-800">
+        <div className="max-w-7xl mx-auto px-4">
+          <div className="h-16 flex justify-between items-center">
+            <div className="flex-shrink-0 sm:hidden flex items-center pl-5">
+              {isMobileNavOpen ? (
+                <FaTimes style={{ color: 'white' }} onClick={toggleMobileNav} />
+              ) : (
+                <FaBars style={{ color: 'white' }} onClick={toggleMobileNav} />
+              )}
             </div>
+            <span className="text-teal-100 text-xl ml-5 font-bold"><Link to="/">VRS_Shopy</Link></span>
 
+            <div className="ml-10 flex items-baseline space-x-4 md:space-x-8">
+             
+
+
+              {/* ........................Category................. */}
+
+              <div className="relative border-2">
+                <button
+                  onMouseEnter={toggleCategoryMenu} // Toggle menu on hover
+                  onMouseLeave={toggleCategoryMenu} // Toggle menu off hover
+                  className="hidden md:block text-2xl text-gray-300 hover:text-white py-3 focus:outline-none"
+                >
+                  <FaList className="inline-block mr-2" />
+                </button>
+                {/* Category menu content */}
+                {isCategoryMenuOpen && (
+                  <div className="  hidden md:block absolute top-full left-0 z-10 py-2 bg-yellow-500 shadow-lg rounded-md">
+                    {categories.map((category) => (
+                      <Link
+                        key={category._id}
+                        to={`/category/${category.slug}`}
+                        className=" px-4 py-2 text-gray-800 hover:bg-gray-200"
+                      >
+                        {category.name}
+                      </Link>
+                    ))}
+                  </div>
+                )}
+              </div>
+
+
+
+
+
+
+
+
+
+
+
+
+              {/* Render other navigation links */}
+              <Link to="/signin" className=" hidden md:block text-xl   text-gray-300 hover:text-white py-3"><FaUser className="inline-block mr-2" /></Link>
+
+              <Link to="/signup" className="hidden md:block text-xl text-gray-300 hover:text-white py-3"><FaUserPlus className="inline-block mr-2" /></Link>
+
+
+
+
+              <Link to="/cart" className="flex items-center text-gray-300 hover:text-white p-4 rounded-md text-xl font-medium">
+                <FaShoppingCart className="mr-2" />
+                Cart
+                <span className="bg-red-500 rounded-full text-white px-2 py-1 ml-2">
+                  {cartItem.length}
+                </span>
+              </Link>
+            </div>
           </div>
-
         </div>
 
+
+
+
+
+
+
+
+
+
+        {/* Mobile navigation menu */}
+        {isMobileNavOpen && (
+          <div className="  block md:hideen pl-8   ">
+            <Link to="/" className="block text-2xl  text-gray-300 hover:text-white py-3"><FaHome className="inline-block mr-2" />Home</Link>
+            <Link to="/signin" className="block text-2xl text-gray-300 hover:text-white py-3"><FaUser className="inline-block mr-2" />Log In</Link>
+            <Link to="/signup" className="block text-2xl text-gray-300 hover:text-white py-3"><FaUserPlus className="inline-block mr-2" />Sign Up</Link>
+           
+           
+           
+           
+            <div className="relative">
+              <button
+        
+                onClick={toggleCategoryMenu}// Toggle menu off hover
+                className="block text-2xl text-gray-300 hover:text-white py-3 focus:outline-none"
+              >
+                <FaList className="inline-block mr-2" />Category
+              </button>
+              {/* Category menu content */}
+              {isCategoryMenuOpen && (
+                <div className="absolute w-40 top-full left-0 z-10 py-2 bg-white  shadow-lg rounded-md">
+                  {categories.map((category) => (
+                    <Link
+                      key={category._id}
+                      to={`/category/${category.slug}`}
+                      className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
+                    >
+                      {category.name}
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+        )}
       </nav>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- Ecommerce/client/src/Pages/Cart.jsx
+++ Ecommerce/client/src/Pages/Cart.jsx
@@ -1,3 +1,4 @@
+
 import React, { useContext, useEffect, useState } from 'react'
 import LayOut from '../Components/Layout/LayOut'
 import { useDispatch, useSelector } from 'react-redux'
@@ -74,18 +75,6 @@ function Cart() {
 
 
 
-    // const handleIncrement = () => {
-    //     // Implement increment logic
-    //     dispatch(incrementQuantity(cartItem._id));
-
-    // };
-
-    // const handleDecrement = (id) => {
-    //     // Implement decrement logic
-    // };
-
-
-
 
 
 
@@ -137,107 +126,56 @@ function Cart() {
 
         <LayOut>
 
-            <div className="min-h-screen bg-gray-100 pt-20">
-                <h1 className="mb-10 text-center text-2xl font-bold">Cart Items</h1>
+           
+<div className="min-h-screen bg-gray-100 pt-20">
+    <h1 className="mb-10 text-center text-2xl font-bold">Cart Items</h1>
 
-
-
-
-
-
-
-
-
-
-
-                <div className="mx-auto max-w-5xl flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-6 xl:px-0">
-
-
-
-
-                    <div className="rounded-lg md:w-2/3">
-
-                        {cartItem.map((item) => (
-
-                            <div className="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
-
-                                <img
-                                    src={`http://localhost:3000/api/product/product-photo/${item._id}`}
-                                    className="card-img-top w-[20%] h-[20%] rounded-lg"
-                                    alt={item.name}
-                                />
-
-                                <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
-
-
-                                    <div className="mt-5 sm:mt-0">
-
-                                        <h2 className="text-lg font-bold text-gray-900">{item.name}</h2>
-                                        <p className="mt-1 text-xs text-gray-700">${item.price}</p>
-                                        <p className="mt-1 text-xs text-gray-700">${item.description.slice(0, 50)}${item.description.length > 50 ? '...' : ''}</p>
-
-
-                                    </div>
-
-
-
-                                    <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
-
-                                        <div className="flex items-center border-gray-100">
-                                            <span className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50" onClick={() => handleDecrement(item._id)}> - </span>
-                                            <input className="h-8 w-8 border bg-white text-center text-xs outline-none" type="number" value={quantities[item._id] || 1} min="1" readOnly />
-                                            <span className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50" onClick={() => handleIncrement(item._id)}> + </span>
-                                        </div>
-
-                                        <div className="flex items-center space-x-4">
-
-                                            <button onClick={() => deleteCart(item._id)} className='text-white bg-red-600 p-2 rounded-lg '>Remove Item <i className='fas fa-trash-alt'></i></button>
-
-                                        </div>
-
-                                    </div>
-                                </div>
+    <div className="mx-auto pb-5 max-w-5xl flex flex-col justify-center px-6 md:flex-row md:space-x-6 xl:px-0">
+        {/* <!-- Cart Items --> */}
+        <div className="md:w-2/3 rounded m-2">
+            {cartItem.map((item) => (
+                <div className="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
+                    <img src={`http://localhost:3000/api/product/product-photo/${item._id}`} className="card-img-top w-1/4 h-auto rounded-lg mb-4 sm:mb-0 sm:w-1/6" alt={item.name} />
+                    <div className="sm:ml-4 sm:flex sm:flex-col sm:w-full sm:justify-between">
+                        <div className="mt-5">
+                            <h2 className="text-lg font-bold text-gray-900">{item.name}</h2>
+                            <p className="mt-1 text-xs text-gray-700">${item.price}</p>
+                            <p className="mt-1 text-xs text-gray-700">{item.description.slice(0, 50)}{item.description.length > 50 ? '...' : ''}</p>
+                        </div>
+                        <div className="mt-4 sm:mt-0">
+                            <div className="flex justify-between items-center border-gray-100">
+                                <span className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50" onClick={() => handleDecrement(item._id)}>-</span>
+                                <input className="h-8 w-8 border bg-white text-center text-xs outline-none" type="number" value={quantities[item._id] || 1} min="1" readOnly />
+                                <span className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50" onClick={() => handleIncrement(item._id)}>+</span>
                             </div>
-
-
-
-
-
-
-                        ))}
-
+                            <button onClick={() => deleteCart(item._id)} className="mt-4 sm:mt-2 text-white bg-red-600 p-2 rounded-lg w-full">Remove Item <i className="fas fa-trash-alt"></i></button>
+                        </div>
                     </div>
-
-
-
-
-
-                    <div className="mt-6 md:mt-0 rounded-lg border bg-white p-6 shadow-md md:w-1/3">
-                        <div className="mb-2 flex justify-between">
-                            <p className="text-gray-700">Subtotal</p>
-                            <p className="text-gray-700">${totalAmount}</p>
-                        </div>
-                        <div className="flex justify-between">
-                            <p className="text-gray-700">Shipping</p>
-                            <p className="text-gray-700">${shipping}</p>
-                        </div>
-                        <hr className="my-4" />
-                        <div className="flex justify-between">
-                            <p className="text-lg font-bold">Total</p>
-                            <div className="">
-                                <p className="mb-1 text-lg font-bold">${grandTotal}</p>
-                                <p className="text-sm text-gray-700">including VAT</p>
-                            </div>
-                        </div>
-                        <button className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">Check out</button>
-                    </div>
-
-
-
-
                 </div>
-
+            ))}
+        </div>
+        {/* <!-- Cart Summary --> */}
+        <div className="mt-6 md:mt-0 md:w-1/3 bg-white h-[50%] p-3 rounded-lg">
+            <div className="mb-2 flex justify-between">
+                <p className="text-gray-700">Subtotal</p>
+                <p className="text-gray-700">${totalAmount}</p>
             </div>
+            <div className="flex justify-between">
+                <p className="text-gray-700">Shipping</p>
+                <p className="text-gray-700">${shipping}</p>
+            </div>
+            <hr className="my-4" />
+            <div className="flex justify-between">
+                <p className="text-lg font-bold">Total</p>
+                <div className="">
+                    <p className="mb-1 text-lg font-bold">${grandTotal}</p>
+                    <p className="text-sm text-gray-700">including VAT</p>
+                </div>
+            </div>
+            <button className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">Check out</button>
+        </div>
+    </div>
+</div>
 
         </LayOut>
 
@@ -247,3 +185,10 @@ function Cart() {
 }
 
 export default Cart
+
+
+
+
+
+
+
