994b5da974b6e63aade598a20ac392975fb9e4b4
rachit <curiousrachit.26@gmail.com>
2024-04-02 22:49:45 +0530

    completed

--- Ecommerce/client/src/Pages/SignIn.jsx
+++ Ecommerce/client/src/Pages/SignIn.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
 import mycontext from '../Context/myContext.jsx';
 import clsx from 'clsx';
+import LayOut from '../Components/Layout/LayOut.jsx';
 function Login() {
     const ContextData = useContext(mycontext);
     const { auth, setAuth } = ContextData;
@@ -50,42 +51,46 @@ function Login() {
     }
 
     return (
-        <div className=' flex justify-center items-center h-screen'>
-            <div className=' bg-[#d2cbbf] shadow-md px-10 py-10 rounded-xl '>
-                <div className="">
-                    <h1 className='text-center text-black text-xl mb-4 font-bold'>Login</h1>
-                </div>
-                <div>
-                    <input
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        type="email"
-                        name='email'
-                        className=' bg-[#beb9b1] border border-green-700 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-black placeholder:text-black outline-none'
-                        placeholder='Email'
-                    />
-                </div>
-                <div>
-                    <input
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        type="password"
-                        className='bg-[#beb9b1] border border-green-700 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-black placeholder:text-black outline-none'
-                        placeholder='Password'
-                    />
-                </div>
-                <div className=' flex justify-center mb-3'>
-                    <button
-                        onClick={loginHandle}
-                        className=' bg-green-700 w-full text-white font-bold  px-2 py-2 rounded-lg'>
-                        Login
-                    </button>
-                </div>
-                <div>
-                    <h2 className='text-black'>Don't have an account <Link className=' text-red-700 font-bold' to={'/signup'}>Signup</Link></h2>
+        <LayOut>
+
+
+            <div className=' flex bg-black justify-center items-center h-screen'>
+                <div className=' bg-[#d2cbbf]  border-2 border-teal-400 shadow-md px-10 py-10 rounded-xl '>
+                    <div className="">
+                        <h1 className='text-center text-black text-xl mb-4 font-bold'>Login</h1>
+                    </div>
+                    <div>
+                        <input
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            type="email"
+                            name='email'
+                            className=' bg-[#beb9b1] border-2 border-teal-500 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-black placeholder:text-black outline-none'
+                            placeholder='Email'
+                        />
+                    </div>
+                    <div>
+                        <input
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            type="password"
+                            className='bg-[#beb9b1] border-2 border-teal-500  mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-black placeholder:text-black outline-none'
+                            placeholder='Password'
+                        />
+                    </div>
+                    <div className=' flex justify-center mb-3'>
+                        <button
+                            onClick={loginHandle}
+                            className=' bg-black w-full text-white font-bold    duration-300 ease-in-out transform hover:scale-90  px-2 py-2 rounded-lg'>
+                            Login
+                        </button>
+                    </div>
+                    <div>
+                        <h2 className='text-black'>Don't have an account <Link className=' text-red-700 font-bold' to={'/signup'}>Signup</Link></h2>
+                    </div>
                 </div>
             </div>
-        </div>
+        </LayOut>
     )
 }
 
