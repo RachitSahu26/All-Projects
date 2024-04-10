d260c371417672aceead14237eacfe7b586d0393
Rachit Sahu <curiousrachit.26@gmail.com>
2024-04-10 10:43:11 +0530

    updated

--- Ecommerce/client/src/Components/PrivateRoute/Private.jsx
+++ Ecommerce/client/src/Components/PrivateRoute/Private.jsx
@@ -3,7 +3,7 @@ import { useContext, useEffect, useState } from "react";
 import mycontext from "../../Context/myContext";
 import { Outlet } from 'react-router-dom';
 import Spinner from "../Spinner/Spinner";
-import { baseUrl } from "../../urls.js";
+// import { baseUrl } from "../../urls.js";
 // import { baseUrl } from "../../../urls";
 
 export const Private = () => {
@@ -15,7 +15,7 @@ export const Private = () => {
         const authCheck = async () => {
             try {
                 if (auth?.token) {
-                    const res = await axios.get(`${baseUrl}/api/auth/user-auth`, {
+                    const res = await axios.get('https://e-commerce-rest-api-fcle.onrender.com/api/auth/user-auth', {
                         headers: {
                             "Authorization": auth?.token
                         }
